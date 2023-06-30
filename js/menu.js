93ea789ea6246068bc00f3c9a35936e2f58c8f31
balegno <mrtinblegno@gmail.com>
2023-06-29 22:05:01 -0300

    agregando fotos e idiomas!

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@ hamburger.addEventListener('click', ()=>{
 
 window.addEventListener('click', e=>{
     if(menu.classList.contains('spread') && e.target != menu && e.target !=hamburger){
+        
         menu.classList.toggle("spread")
     }
 })
